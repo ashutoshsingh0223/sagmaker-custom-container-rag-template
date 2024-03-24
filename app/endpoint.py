5836a46d5d795378dd13480bb2d824916d18077d
Ashutosh Singh <40604544+ashutoshsingh0223@users.noreply.github.com>
2024-03-24 10:12:00 +0100

    Update endpoint.py

--- app/endpoint.py
+++ app/endpoint.py
@@ -26,42 +26,36 @@ async def inference(request: Request):
     print(os.listdir("/opt/ml/model"))
     manifest = await request.json()
 
+    # Type of job, retrieve(for model endpoints)
+    # or index(for batch transform jobs)
     job_type = manifest['job_type']
+    # name for the job
     job_name = manifest['job_name']
-    file_ids = manifest['file_ids']
-    callback_url = manifest['callback_url']
 
+    # A callback URL to update job status
+    callback_url = manifest['callback_url']
+    # Host for elasctic search , used here as vector DB.
     elastic_host = manifest["elastic_host"]
 
-    if job_type == 'embed':
-        query = manifest['query']
-        t = transformer.Transformer(
-            model_cache_dir=constants.MODEL_CACHE_DIR_HF,
-            client=client,
-            project=project,
-            elastic_host=elastic_host,
-            environment=environment
-        )
+    t = processor.Processor(
+                model_cache_dir="/opt/ml/model",
+                elastic_host=elastic_host,
+                environment=environment
+            )
 
+    if job_type == 'retrieve':
+        query = manifest['query']
         docs = t.search(query)
         return {"docs": docs}
     else:
-
         st = time.time()
         import reader
         try:
             documents = manifest['documents']
             r = reader.DocReader(docs=documents)
-            t = processor.Processor(
-                model_cache_dir="/opt/ml/model",
-                client=client,
-                project=project,
-                elastic_host=elastic_host,
-                environment=environment
-            )
+            
             lanchain_docs = r.get_documents()
             t.embed_documents(documents=lanchain_docs)
-
             success = True
             error = ''
         except Exception as e:
@@ -72,25 +66,12 @@ async def inference(request: Request):
 
         callback = {
                         "success": success,
-                        "file_ids": file_ids,
                         "job_name": job_name,
                         "time": et - st,
                         "error": error
                     }
         headers = {"Content-Type": "application/json"}
         requests.post(callback_url, json=callback, headers=headers)
-   
-
-        callback = {
-                        "success": success,
-                        "file_ids": file_ids,
-                        "job_name": job_name,
-                        "time": et - st,
-                        "error": error
-                    }
-        headers = {"Content-Type": "application/json"}
-        requests.post(callback_url, json=callback, headers=headers)
-
 
     return {"message": "finish"}
 
